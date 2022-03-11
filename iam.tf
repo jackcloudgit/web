99c0e2c0cbce720eeab8fb95f100a2775e0d0624
Jagadeesh <jagadeesh@testmail.com>
2022-03-11 23:47:01 +0530

    added

--- iam.tf
+++ iam.tf
@@ -4,11 +4,9 @@ provider "aws" {
     secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-
 resource "aws_iam_user" "example" {
   name  = var.user
 }
-
 
 variable "user" {
    default = "Newharnestest"
@@ -17,17 +15,7 @@ variable "user" {
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
 
-resource "time_sleep" "wait_30_seconds" {
-  
-  depends_on = [aws_iam_user.example]
-
-  create_duration = "30s"
-}
-
 output test {
  
   value = aws_iam_user.example.name
-  depends_on = [time_sleep.wait_30_seconds]
-
-
 }
