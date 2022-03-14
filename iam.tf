72bf5f7e111a7c164ec99128ea9acb35d10ab4d8
Jagadeesh <jagadeesh@testmail.com>
2022-03-14 17:14:09 +0530

    updated timelimit

--- iam.tf
+++ iam.tf
@@ -8,6 +8,13 @@ resource "aws_iam_user" "example" {
   name  = var.user
 }
 
+resource "time_sleep" "wait_30_seconds" {
+  
+  depends_on = [aws_iam_user.example]
+
+  create_duration = "30s"
+}
+
 variable "user" {
    default = "Newharnestest"
 }
@@ -18,4 +25,5 @@ variable "AWS_SECRET_ACCESS_KEY" {}
 output test {
  
   value = aws_iam_user.example.name
+  depends_on = [time_sleep.wait_30_seconds]
 }
