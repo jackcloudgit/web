b02fc0df914ce3cb4e54a4dbd6dd6d70c5a1a2f6
Jagadeesh <jagadeesh@testmail.com>
2022-03-17 16:48:09 +0530

    added new script

--- amiinfra/amiinfra.tf
+++ amiinfra/amiinfra.tf
@@ -30,8 +30,7 @@ resource "aws_autoscaling_group" "asg_test" {
   
   tag {
       key = "Name"
-      value = "${var.name}-web"
-      propagate_at_launch = true
+      value = "web"
   }
 
   timeouts {
