acac6480e162f1f0dad2e23e4ace34d35c3dab69
Jagadeesh <jagadeesh@testmail.com>
2022-03-18 11:46:02 +0530

    updated main

--- amiinfra/amiinfra.tf
+++ amiinfra/amiinfra.tf
@@ -3,9 +3,6 @@ provider "aws" {
     access_key = var.AWS_ACCESS_KEY_ID
     secret_key = var.AWS_SECRET_ACCESS_KEY
 }
-variable "user" {
-   default = "Newharnestest10"
-}
 
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
