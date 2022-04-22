107b6bdf86c28f6d53ebcd79c690b0182b4d3f26
Jagadeesh <jagadeeshmcp@gmail.com>
2022-04-22 14:16:10 +0530

    updated changes

---
+++ test/iam.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region     = "us-east-1"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_iam_user" "example" {
+  name  = var.user
+}
+
+resource "time_sleep" "wait_30_seconds" {
+  
+  depends_on = [aws_iam_user.example]
+
+  create_duration = "30s"
+}
+
+variable "user" {
+   default = "Newharnestesttest89"
+}
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+output test {
+ 
+  value = aws_iam_user.example.name
+  depends_on = [time_sleep.wait_30_seconds]
+}
